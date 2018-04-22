6c5c196937c5b4201df7486168de3d72fb726f29
lycantropus <mechanical.maggot.0@gmail.com>
2018-04-22 18:58:13 +0100

    code, first postman, docker file, requirements

---
+++ genesis_block.py
@@ -0,0 +1,12 @@
+import datetime as date
+from block import Block
+
+
+def create_genesis_block():
+    return Block(0,
+                 date.datetime.now(),
+                 {
+                     "proof-of-work": 9,
+                     "transactions": None
+                 },
+                 "0")
